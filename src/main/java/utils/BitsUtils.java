646c50625d91ba6c9977180a27eefbf72cdc4b2d
zhangkai <chinazhangk@gmail.com>
2021-10-12 00:11:15 +0800

    [Math][fix]:修复getHigh函数计算位数的错误。补全了一些新的单元测试

--- src/main/java/utils/BitsUtils.java
+++ src/main/java/utils/BitsUtils.java
@@ -1,43 +1,76 @@
 package utils;
 
 public class BitsUtils {
-    public static int getLowerSafe(int number, int byteCount) {
+    public final static int INT_MAX_BITS_COUNT = 32;
+    public final static int LONG_MAX_BITS_COUNT = 64;
+    public final static int FLOAT_MAX_BITS_COUNT = 32;
+    public final static int DOUBLE_MAX_BITS_COUNT = 64;
+
+    public static int getLower(int number, int byteCount) {
         int baseNum = (int)Math.pow(2, byteCount) - 1;
         return number & baseNum;
     }
 
-    public static int getHighSafe(int number, int byteCount) {
-        return number >>> byteCount;
+    /**
+     * 获取高n位。
+     * 包括符号位，例如：获取高1位则是符号位
+     * @param number 目标数字
+     * @param byteCount 要截取多少位
+     * @return 截取@byteCount位后的结果
+     */
+    public static int getHigh(int number, int byteCount) {
+        return number >>> (INT_MAX_BITS_COUNT - byteCount);
     }
 
-    public static int getLowerSafe(float number, int byteCount) {
+    public static int getLower(float number, int byteCount) {
         int intBits = Float.floatToIntBits(number);
         int baseNum = (int)Math.pow(2, byteCount) - 1;
         return intBits & baseNum;
     }
 
-    public static int getHighSafe(float number, int byteCount) {
+    /**
+     * 获取高n位。
+     * 包括符号位，例如：获取高1位则是符号位
+     * @param number 目标数字
+     * @param byteCount 要截取多少位
+     * @return 截取@byteCount位后的结果
+     */
+    public static int getHigh(float number, int byteCount) {
         int intBits = Float.floatToIntBits(number);
-        return intBits >>> byteCount;
+        return intBits >>> (FLOAT_MAX_BITS_COUNT - byteCount);
     }
 
-    public static long getLowerSafe(long number, int byteCount) {
+    public static long getLower(long number, int byteCount) {
         long baseNum = (long)Math.pow(2, byteCount) - 1;
         return number & baseNum;
     }
 
-    public static long getHighSafe(long number, int byteCount) {
-        return number >>> byteCount;
+    /**
+     * 获取高n位。
+     * 包括符号位，例如：获取高1位则是符号位
+     * @param number 目标数字
+     * @param byteCount 要截取多少位
+     * @return 截取@byteCount位后的结果
+     */
+    public static long getHigh(long number, int byteCount) {
+        return number >>> (LONG_MAX_BITS_COUNT - byteCount);
     }
 
-    public static long getLowerSafe(double number, int byteCount) {
+    public static long getLower(double number, int byteCount) {
         long longBits = Double.doubleToLongBits(number);
         long baseNum = (long)Math.pow(2, byteCount) - 1;
         return longBits & baseNum;
     }
 
-    public static long getHighSafe(double number, int byteCount) {
+    /**
+     * 获取高n位。
+     * 包括符号位，例如：获取高1位则是符号位
+     * @param number 目标数字
+     * @param byteCount 要截取多少位
+     * @return 截取@byteCount位后的结果
+     */
+    public static long getHigh(double number, int byteCount) {
         long longBits = Double.doubleToLongBits(number);
-        return longBits >>> byteCount;
+        return longBits >>> (DOUBLE_MAX_BITS_COUNT - byteCount);
     }
 }
--- src/test/java/utils/TestBitsUtils.java
+++ src/test/java/utils/TestBitsUtils.java
@@ -5,47 +5,94 @@ import org.junit.Test;
 public class TestBitsUtils {
     @Test
     public void testLowHighBit() {
-        testLowHighBit(Long.MAX_VALUE, 32);
-        System.out.println("============");
-        testLowHighBit(Long.MIN_VALUE, 32);
-        System.out.println("============");
-        testLowHighBit(0, 16);
-        System.out.println("============");
-        testLowHighBit(-1, 16);
-        System.out.println("============");
-        testLowHighBit(10000, 16);
-        System.out.println("============");
-        testLowHighBit(1024, 16);
-        System.out.println("============");
         testLowHighBit(Integer.MAX_VALUE, 16);
-        System.out.println("============");
+        testLowHighBit(Integer.MAX_VALUE, 0);
+        testLowHighBit(Integer.MAX_VALUE, 1);
+
         testLowHighBit(Integer.MIN_VALUE, 16);
-        System.out.println("============");
+        testLowHighBit(Integer.MIN_VALUE, 0);
+        testLowHighBit(Integer.MIN_VALUE, 1);
+
+
+        testLowHighBit(0, 16);
+        testLowHighBit(0, 1);
+        testLowHighBit(0, 0);
+
+        testLowHighBit(-1, 16);
+        testLowHighBit(-1, 1);
+        testLowHighBit(-1, 0);
+
         testLowHighBit(65535, 16);
+        testLowHighBit(65535, 1);
+        testLowHighBit(65535, 0);
+
+        testLowHighBit(10000, 16);
+        testLowHighBit(10000, 1);
+        testLowHighBit(10000, 0);
+
+        testLowHighBit(1024, 16);
+        testLowHighBit(1024, 1);
+        testLowHighBit(1024, 0);
+
+        testLowHighBit(Long.MAX_VALUE, 32);
+        testLowHighBit(Long.MAX_VALUE, 1);
+        testLowHighBit(Long.MAX_VALUE, 0);
+
+        testLowHighBit(Long.MIN_VALUE, 32);
+        testLowHighBit(Long.MIN_VALUE, 1);
+        testLowHighBit(Long.MIN_VALUE, 0);
+
+        testLowHighBit(Float.MAX_VALUE, 32);
+        testLowHighBit(Float.MAX_VALUE, 1);
+        testLowHighBit(Float.MAX_VALUE, 0);
+
+        testLowHighBit((float) 10000.001, 16);
+        testLowHighBit((float) 10000.001, 1);
+        testLowHighBit((float) 10000.001, 0);
+
+        testLowHighBit(Float.MIN_VALUE, 32);
+        testLowHighBit(Float.MIN_VALUE, 1);
+        testLowHighBit(Float.MIN_VALUE, 0);
+
+        testLowHighBit(Double.MAX_VALUE, 32);
+        testLowHighBit(Double.MAX_VALUE, 1);
+        testLowHighBit(Double.MAX_VALUE, 0);
+
+        testLowHighBit(1000000.001, 16);
+        testLowHighBit(1000000.001, 1);
+        testLowHighBit(1000000.001, 0);
+
+        testLowHighBit(Long.MIN_VALUE, 32);
+        testLowHighBit(Long.MIN_VALUE, 1);
+        testLowHighBit(Long.MIN_VALUE, 0);
     }
 
     private void testLowHighBit(int number, int bitCount) {
-        System.out.println("number：" + number);
+        System.out.println("===========Int=============");
+        System.out.println("number：" + number + " bitCount:" + bitCount);
         System.out.println("HexString：" + Integer.toHexString(number));
-        System.out.println("LowHexString：" + Integer.toHexString(BitsUtils.getLowerSafe(number, bitCount)));
-        System.out.println("HighHexString：" + Integer.toHexString(BitsUtils.getHighSafe(number, bitCount)));
+        System.out.println("LowHexString：" + Integer.toHexString(BitsUtils.getLower(number, bitCount)));
+        System.out.println("HighHexString：" + Integer.toHexString(BitsUtils.getHigh(number, bitCount)));
     }
     private void testLowHighBit(float number, int bitCount) {
-        System.out.println("number：" + number);
-        System.out.println("HexString：" + Float.toHexString(number));
-        System.out.println("LowHexString：" + Float.toHexString(BitsUtils.getLowerSafe(number, bitCount)));
-        System.out.println("HighHexString：" + Float.toHexString(BitsUtils.getHighSafe(number, bitCount)));
+        System.out.println("===========Float=============");
+        System.out.println("number：" + number + " bitCount:" + bitCount);
+        System.out.println("HexString：" + Integer.toHexString(Float.floatToIntBits(number)));
+        System.out.println("LowHexString：" + Integer.toHexString(BitsUtils.getLower(number, bitCount)));
+        System.out.println("HighHexString：" + Integer.toHexString(BitsUtils.getHigh(number, bitCount)));
     }
     private void testLowHighBit(long number, int bitCount) {
-        System.out.println("number：" + number);
+        System.out.println("===========Long=============");
+        System.out.println("number：" + number + " bitCount:" + bitCount);
         System.out.println("HexString：" + Long.toHexString(number));
-        System.out.println("LowHexString：" + Long.toHexString(BitsUtils.getLowerSafe(number, bitCount)));
-        System.out.println("HighHexString：" + Long.toHexString(BitsUtils.getHighSafe(number, bitCount)));
+        System.out.println("LowHexString：" + Long.toHexString(BitsUtils.getLower(number, bitCount)));
+        System.out.println("HighHexString：" + Long.toHexString(BitsUtils.getHigh(number, bitCount)));
     }
     private void testLowHighBit(double number, int bitCount) {
-        System.out.println("number：" + number);
-        System.out.println("HexString：" + Double.toHexString(number));
-        System.out.println("LowHexString：" + Double.toHexString(BitsUtils.getLowerSafe(number, bitCount)));
-        System.out.println("HighHexString：" + Double.toHexString(BitsUtils.getHighSafe(number, bitCount)));
+        System.out.println("===========Double=============");
+        System.out.println("number：" + number + " bitCount:" + bitCount);
+        System.out.println("HexString：" + Long.toHexString(Double.doubleToLongBits(number)));
+        System.out.println("LowHexString：" + Long.toHexString(BitsUtils.getLower(number, bitCount)));
+        System.out.println("HighHexString：" + Long.toHexString(BitsUtils.getHigh(number, bitCount)));
     }
 }
